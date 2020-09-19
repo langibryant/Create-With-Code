6ef4cd210beb73c5984a85d7fe59597e987197b5
Tauelangi Bryant <10865140@my.uvu.edu>
2020-09-19 10:17:26 -0600

    added new csripts for camera and player

---
+++ Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    public GameObject Player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Player.transform.position + new Vector3(0, 5, -7);
+    }
+}
--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    public float speed = 5.0f;
+
+        // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }

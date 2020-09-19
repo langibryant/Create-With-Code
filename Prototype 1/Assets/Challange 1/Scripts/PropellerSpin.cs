112669f611ffc454e149894b5323681a41097ff9
Tauelangi Bryant <10865140@my.uvu.edu>
2020-09-19 14:25:42 -0600

    added completed plane challenge

---
+++ Prototype 1/Assets/Challange 1/Scripts/PropellerSpin.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellerSpin : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.back * 1000 * Time.deltaTime);
+    }
+}
--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -14,7 +14,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-        fowardInput = Input.Get Axis("Vertical");
+        fowardInput = Input.GetAxis("Vertical");
         transform.Translate(Vector3.forward * Time.deltaTime * speed * fowardInput);
         transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
     }

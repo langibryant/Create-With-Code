1036421651bbd8a64d6daff9d87629a02488618a
Tauelangi Bryant <10865140@my.uvu.edu>
2020-09-19 13:43:51 -0600

    finishing up the car challenge

--- Prototype 1/Assets/Scripts/FollowPlayer.cs
+++ Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -6,10 +6,11 @@ public class FollowPlayer : MonoBehaviour
 {
 
     public GameObject Player;
+    private Vector3 offset = new Vector3(0f, 5f, -7f);
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Player.transform.position + new Vector3(0, 5, -7);
+        transform.position = Player.transform.position + offset;
     }
 }
--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public float speed = 5.0f;
+    public float speed = 20f;
+    public float turnSpeed = 45f;
+    public float horizontalInput;
+    public float fowardInput;
 
         // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        horizontalInput = Input.GetAxis("Horizontal");
+        fowardInput = Input.Get Axis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * fowardInput);
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
